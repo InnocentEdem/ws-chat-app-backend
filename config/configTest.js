84953ff76cfc62713250d24df38d5b7333c90744
Innocent Edem <82113948+InnocentEdem@users.noreply.github.com>
2022-05-04 17:12:57 +0000

    defined config test files

---
+++ config/configTest.js
@@ -0,0 +1,12 @@
+
+require ("dotenv").config();
+
+module.exports = {
+    development: {
+    username: 'bwvtmegs',
+    password: 'LQdxYaqxc9qXlkOquGgSpVZTGpN3dtrS',
+    database: 'bwvtmegs',
+    host: 'ella.db.elephantsql.com',
+    dialect: "postgres",
+  }
+}
